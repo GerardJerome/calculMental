f5ca88a02c65df221d761ce27b5f8e09f233ea1a
jerom <jerome.gerard@ext.adeo.com>
2021-12-06 10:41:58 +0100

    première view

---
+++ app/src/main/java/com/example/calculemental/view/PartieEnCoursActivity.java
@@ -0,0 +1,16 @@
+package com.example.calculemental.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.calculemental.R;
+
+public class PartieEnCoursActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_partie_en_cours);
+    }
+}
